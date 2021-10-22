1a686836609b303c504e32c1b01d63449a5cc697
stetilf <91318936+stetilf@users.noreply.github.com>
2021-10-22 12:05:19 +0200

    Update tetris.cpp

--- tetris.cpp
+++ tetris.cpp
@@ -13,15 +13,17 @@ float pitch = 0.0F;
 float roll  = 0.0F;
 float yaw   = 0.0F;
 
-int canvas[200];
+uint16_t canvas[200];
 int canvasX=12;
 int canvasY=2;
 int i=0;
 int j=0;
 int rect_size=10;
+int position=0;
 
 //135*240
 void setup() {
+   canvas[10]=0xffff;
    M5.begin();
    M5.Lcd.fillScreen(BLACK);
    pinMode(10, OUTPUT);
@@ -40,7 +42,7 @@ M5.Lcd.setCursor(0,0);
 M5.Lcd.setTextColor(WHITE);
 M5.Lcd.setTextSize(5);
  
-M5.Lcd.println("TEST");  
+//M5.Lcd.println("TEST");  
 /*M5.Lcd.drawRect(0,0,10,10,RED);  
 M5.Lcd.drawRect(1,1,98,98,GREEN);  
 M5.Lcd.drawRect(2,2,97,97,BLUE);  */
@@ -51,6 +53,24 @@ for(j=0;j<22; j++)
    {
    for(i=0;i<11; i++)
    {
+      M5.update();
+    if(M5.BtnA.isPressed()) {
+    Serial.println("button A pressed");
+    i=0;
+    j=0;
+    position=0;
+    canvasX=12;
+    canvasY=2;
+    M5.Lcd.fillScreen(BLACK);
+    }
+    
+    if (canvas[position]==0xffff)
+    {
+    M5.Lcd.drawRect(canvasX+(rect_size*i),canvasY+10,rect_size,rect_size,0xffff);
+    } else M5.Lcd.drawRect(canvasX+(rect_size*i),canvasY+10,rect_size,rect_size,0xcccc);
+
+
+    /*
     if(j%2==0)
     {
     if (i%2==0)
@@ -63,20 +83,13 @@ for(j=0;j<22; j++)
     M5.Lcd.drawRect(canvasX+(rect_size*i),canvasY+10,rect_size,rect_size,0xffff);
     } else 
     M5.Lcd.drawRect(canvasX+(rect_size*i),canvasY+10,rect_size,rect_size,0xffff);
+    */
     Serial.print(i);
     Serial.print(" ");
     Serial.println(j);
-    M5.update();
-    if(M5.BtnA.isPressed()) {
-    Serial.println("button A pressed");
-    i=0;
-    j=0;
-    M5.Lcd.fillScreen(BLACK);
-    }
-    
+    position++;
     delay(200);
     
-   
 
    }
    canvasY=canvasY+10;
