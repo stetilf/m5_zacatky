583ad53485511ae0a9e6de23c3f5f672635f07b6
stetilf <91318936+stetilf@users.noreply.github.com>
2021-11-26 12:59:27 +0100

    Update pong.cpp

--- pong.cpp
+++ pong.cpp
@@ -27,10 +27,48 @@ void loop() {
      ballY--;
    }
 //řešení kolize balonu s okrajem obrazovky
-  if (ballDir==0 && ballX>130) //směr je vpravo nahoru
+   
+          if (ballDir==0 && ballX>130) //směr doprava nahoru && kolize vpravo
    {
      ballDir=3;
+     ballDir+=10;                        //změna směru na vlevo nahoru
+   } else if (ballDir==0 && ballY<5)   //směr doprava nahoru && kolize nahoře
+   {
+     ballDir=1;
+     ballDir+=10;                        //změna směru na vpravo dolů
+   } else if (ballDir==1 && ballY>235) //směr doprava dolů && kolize dole
+   {  
+     ballDir=0;
+     ballDir+=10;                        //změna směru na vpravo nahoru
+   } else if (ballDir==1 && ballX>130) //směr doprava dolů && kolize vpravo
+   {
+     ballDir=2;
+     ballDir+=10;                        //změna směru na vlevo dolů
+   } else if (ballDir==2 && ballY<235) //směr vlevo dolů && kolize dole
+   {
+     ballDir=3;
+     ballDir+=10;                        //změna směru na vlevo nahoru
+   } else if (ballDir==2 && ballX<5)   //směr vlevo dolů && kolize dole
+   {
+     ballDir=1;
+     ballDir+=10;                        //změna směru na vpravo dolů
+   } else if (ballDir==3 && ballY<5)   //směr vlevo nahoru && kolize nahoře
+   {
+     ballDir=2;
+     ballDir+=10;                        //změna směru na vlevo dolů
+   } else if (ballDir==3 && ballX<5)   //směr vlevo nahoru && kolize vlevo
+   {
+     ballDir=0;
+     ballDir+=10;                        //změna směru na vpravo nahoru
    }
+
+  if (ballDir>=10) ballDir-=10;
+
+  Serial.println(ballX);
+  Serial.println(ballY);
+  Serial.println(ballDir);
+  Serial.println("");
+
    M5.Lcd.drawCircle(ballX,ballY,3,WHITE);
    M5.update();   // update button state
    if(M5.BtnA.isPressed()) {
@@ -40,8 +78,9 @@ void loop() {
    if(M5.BtnB.isPressed()) {
       pX-=1;
       Serial.println("Key B");
+      delay(10000);
    }
     M5.Lcd.drawRect(pX,pY,40,8,WHITE);
-   delay(50);
+   delay(10);
    M5.Lcd.fillScreen(BLACK);
 }
